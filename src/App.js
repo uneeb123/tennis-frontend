c1b4a4ca95e9c483bddf5367e2f79386a2ac9944
Uneeb Agha <uneeb.agha@gmail.com>
2022-10-13 13:21:19 -0400

    rainbow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 function App() {
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+    [
+      alchemyProvider({ apiKey: 'SrDO2MifdoOqynE5FKmWWy_oxNWFR0Jo' }),
+      publicProvider()
+    ]
+  );
+  
+  const { connectors } = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains
+  });
+  
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div className="App">
+          <div className="nav">
+            <ConnectButton />
+          </div>
+          <header className="App-header">
+            <p>
+              Welcome to Tennis Finance!
+            </p>
+          </header>
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
